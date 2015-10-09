4e764b1533d59b8b4fd26402b625c825a984527e
Reuben Cummins <S00132290@mail.itsligo.ie>
2015-10-09 10:47:31 +0100

    MVC project template

---
+++ MVCThingumy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCThingumy.MvcApplication" Language="C#" %>
---
+++ MVCThingumy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCThingumy.Startup))]
+namespace MVCThingumy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
